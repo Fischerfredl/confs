4abb9179da3371afd5a793cc61eb55293b2944d2
Alfred Melch <alfred.melch@gmx.de>
2018-06-20 16:01:20 +0200

    add different markers

--- web/src/components/map-utils.js
+++ web/src/components/map-utils.js
@@ -5,6 +5,9 @@ import { connect } from 'pwa-helpers/connect-mixin.js'
 
 import { store} from '../store.js'
 
+import iconset from '../lib/markers.js'
+
+
 class MapUtils extends connect(store)(LitElement) {
   _render({ data }){
     this.refresh(data)
@@ -30,7 +33,14 @@ class MapUtils extends connect(store)(LitElement) {
 
   refresh(data) {
     this.markers.clearLayers()
-    this.markers.addLayer(L.geoJson(data))
+    const geoJsonLayer = L.geoJson(data, {
+      pointToLayer: (feature, latlng) => {
+        console.log(feature.properties.year, feature.properties.startDate, feature.properties.endDate)
+        return L.marker(latlng, {icon: iconset[feature.properties.topic]})
+      }
+    })
+
+    this.markers.addLayer(geoJsonLayer)
   }
 
   static get properties() {
