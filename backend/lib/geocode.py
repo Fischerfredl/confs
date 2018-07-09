8e14a3d092a4a93309ad9d1620b5195f5145c9ea
Alfred Melch <alfred.melch@gmx.de>
2018-07-09 13:00:39 +0200

    refined geocoding is worse than before

--- backend/lib/geocode.py
+++ backend/lib/geocode.py
@@ -12,14 +12,12 @@ patches = {
 }
 
 
-def nominatim_geocode(query, only_cities=False):
+def nominatim_geocode(query):
     params = {
         'q': query,
         'format': 'json',
         'limit': '1'
     }
-    if only_cities:
-        params['featuretype'] = 'city'
     headers = {
         'Accept-Language': 'en-US,en;q=0.9'
     }
@@ -34,7 +32,7 @@ def geocode_conference(conf):
     city = conf.get('city')
     country = conf.get('country')
 
-    cache_key = f'query-v3.1-{country}-{city}'
+    cache_key = f'query-v3.2-{country}-{city}'
     cached = get_cache(cache_key)
     if cached is not None:
         return cached
@@ -43,13 +41,9 @@ def geocode_conference(conf):
     city = patches.get(city) or city
 
     # try geocoding
-    data = nominatim_geocode(city + ', ' + country, only_cities=True)
+    data = nominatim_geocode(city + ', ' + country)
     if len(data) == 0:
-        data = nominatim_geocode(city, only_cities=True)
-    if len(data) == 0:
-        data = nominatim_geocode(city + ', ' + country, only_cities=False)
-    if len(data) == 0:
-        data = nominatim_geocode(city, only_cities=False)
+        data = nominatim_geocode(city)
 
     if len(data) == 0:
         # failed
